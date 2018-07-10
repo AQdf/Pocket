119d572b3731574ad88606dbda97fccce37001f5
AQdf <newerainua@gmail.com>
2018-07-10 22:36:05 +0300

    Configure connection string

--- src/Sho.Pocket.Web/Startup.cs
+++ src/Sho.Pocket.Web/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Sho.Pocket.BLL.Services;
+using Sho.Pocket.Core.Abstractions;
 
 namespace Sho.Pocket.Web
 {
@@ -25,7 +19,11 @@ namespace Sho.Pocket.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            string connectionString = Configuration.GetConnectionString("PocketLocalDbConnection");
+
+            services.AddScoped<ISummaryService, SummaryService>();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
