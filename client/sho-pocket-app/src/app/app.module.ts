f22d32304ce6bacaa8f96af3971b542ce2f91950
AQdf <newerainua@gmail.com>
2020-01-19 21:29:48 +0200

    Remove HttpModule

--- client/sho-pocket-app/src/app/app.module.ts
+++ client/sho-pocket-app/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http'
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule} from '@angular/forms'
 import { ToastrModule } from 'ngx-toastr';
@@ -54,7 +53,6 @@ import { ExchangeRatesComponent } from './components/exchange-rates/exchange-rat
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule,
     HttpClientModule,
     FormsModule,
     ToastrModule.forRoot(),
