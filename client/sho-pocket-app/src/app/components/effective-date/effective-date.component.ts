88517292290f4946e38c69862b1a8fff7b45ed7f
AQdf <newerainua@gmail.com>
2020-02-25 20:49:04 +0200

    Integrate Effective Date component to Balances page

--- client/sho-pocket-app/src/app/components/effective-date/effective-date.component.ts
+++ client/sho-pocket-app/src/app/components/effective-date/effective-date.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { from } from 'rxjs';
 import { distinct, map, toArray, filter } from 'rxjs/operators';
 
@@ -11,10 +11,17 @@ import { BalanceService } from '../../services/balance.service'
 })
 export class EffectiveDateComponent implements OnInit {
 
+  @Output() effectiveDateEvent = new EventEmitter<string>();
+
+  selectedEffectiveDate: string;
   effectiveDates: Date[];
   years: number[];
   months: number[];
   days: number[];
+
+  selectingYear: boolean;
+  selectingMonth: boolean;
+  selectingDay: boolean;
 
   selectedYear: number;
   selectedMonth: number;
@@ -30,19 +37,37 @@ export class EffectiveDateComponent implements OnInit {
   getEffectiveDates() {
     this.balanceService.getEffectiveDates().subscribe((effectiveDates: Date[]) => {
       this.effectiveDates = effectiveDates;
-
-      from(this.effectiveDates).pipe(
-        map(date => new Date(date).getFullYear()),
-        distinct(),
-        toArray(),
-      ).subscribe(years => {
-        this.years = years.sort((a,b) => b - a);
-      }); 
+      this.setToday();
     });
+  }
+
+  setToday() {
+    if (this.effectiveDates.length > 0) {
+      let latestDate = new Date(this.effectiveDates[0]);
+      this.selectedEffectiveDate =  new Date(this.effectiveDates[0]).toISOString();
+
+      this.setYears();
+      this.selectYear(latestDate.getFullYear());
+      this.selectMonth(latestDate.getMonth());
+      this.selectDay(latestDate.getDate());
+    }
+  }
+
+  setYears() {
+    from(this.effectiveDates).pipe(
+      map(date => new Date(date).getFullYear()),
+      distinct(),
+      toArray(),
+    ).subscribe(years => {
+      this.years = years.sort((a,b) => b - a);
+    }); 
   }
 
   selectYear(year: number) {
     this.selectedYear = year;
+    this.selectedMonth = null;
+    this.selectedDay = null;
+    this.selectingMonth = true;
 
     from(this.effectiveDates).pipe(
       filter(date => new Date(date).getFullYear() === this.selectedYear),
@@ -56,6 +81,8 @@ export class EffectiveDateComponent implements OnInit {
 
   selectMonth(month: number) {
     this.selectedMonth = month;
+    this.selectedDay = null;
+    this.selectingDay = true;
 
     from(this.effectiveDates).pipe(
       filter(dateStr => {
@@ -72,11 +99,29 @@ export class EffectiveDateComponent implements OnInit {
 
   selectDay(day: number) {
     this.selectedDay = day;
+    this.selectingYear = false;
+    this.selectingMonth = false;
+    this.selectingDay = false;
+    let date = new Date(this.selectedYear, this.selectedMonth, this.selectedDay);
+    this.selectedEffectiveDate = date.toISOString();
+    this.effectiveDateEvent.emit(this.selectedEffectiveDate);
   }
 
-  clear() {
-    this.selectedYear = null;
-    this.selectedMonth = null;
-    this.selectedDay = null;
+  onYearClick() {
+    this.selectingYear = !this.selectingYear;
+    this.selectingMonth = false;
+    this.selectingDay = false;
+  }
+
+  onMonthClick() {
+    this.selectingMonth = !this.selectingMonth;
+    this.selectingYear = false;
+    this.selectingDay = false;
+  }
+
+  onDayClick() {
+    this.selectingDay = !this.selectingDay;
+    this.selectingYear = false;
+    this.selectingMonth = false;
   }
 }
