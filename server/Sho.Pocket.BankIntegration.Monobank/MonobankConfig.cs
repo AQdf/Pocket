01dae405f944130a025d3e952bc7c387f37b328a
AQdf <newerainua@gmail.com>
2020-03-04 20:03:06 +0200

    Move Privatbank and Monobank bank integration to nuget

--- server/Sho.Pocket.BankIntegration.Monobank/MonobankConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sho.BankIntegration.Monobank
-{
-    public static class MonobankConfig
-    {
-        public const string BANK_NAME = "Monobank";
-
-        public const string BANK_API_URL = "https://api.monobank.ua";
-    }
-}
--- server/Sho.Pocket.BankIntegration.PrivatBank/PrivatbankConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sho.BankIntegration.Privatbank
-{
-    public static class PrivatbankConfig
-    {
-        public const string BANK_NAME = "Privatbank";
-
-        public const string BANK_API_URL = "https://api.privatbank.ua/p24api";
-    }
-}
